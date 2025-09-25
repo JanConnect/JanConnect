a6667385eaf4d5dcb49ba5602fa9074e7fc69a01
Aditi <aditinehadeepak@gmail.com>
2025-09-25 19:17:30 +0530

    Setup GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,16 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-   server: {
+  base: "/JanConnect/", 
+  server: {
     port: 5173,
     open: true,
     proxy: {
       '/api': {
-        target: 'http://localhost:8000', // Changed from 3000 to 8000
+        target: 'http://localhost:8000',
         changeOrigin: true,
         secure: false,
       }
-    } 
+    }
   },
 })
